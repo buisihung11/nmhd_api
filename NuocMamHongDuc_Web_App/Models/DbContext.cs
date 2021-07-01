db7b69ff10a4ac43e34073afb29ca7f75ef0266c
HungHCC <buisihung321@gmail.com>
2021-07-01 10:20:06 +0700

    Init project

---
+++ NuocMamHongDuc_Web_App/Models/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NuocMamHongDuc_Web_App.Models
+{
+    public class NuocMamDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public NuocMamDbContext(DbContextOptions<NuocMamDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
