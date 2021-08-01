0ab951149eaaf567e2584cd944d27d98e762f511
HungHCC <buisihung321@gmail.com>
2021-08-01 17:13:38 +0700

    Add footerImage prop

---
+++ NMHD_DataAccess/Migrations/20210801101021_ADdfooterimage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NMHD_DataAccess.Migrations
+{
+    public partial class ADdfooterimage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FooterImageUrl",
+                table: "StoreConfigs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FooterImageUrl",
+                table: "StoreConfigs");
+        }
+    }
+}
--- NMHD_DataAccess/Models/StoreConfig.cs
+++ NMHD_DataAccess/Models/StoreConfig.cs
@@ -8,7 +8,7 @@ namespace NMHD_DataAccess.Models
 {
     public class StoreConfig
     {
-        public StoreConfig(int id, string providerName1, string providerName2, string providerPhone1, string providerPhone2, string providerMail1, string providerMail2, DateTime startTime, DateTime endTime, string websiteURL)
+        public StoreConfig(int id, string providerName1, string providerName2, string providerPhone1, string providerPhone2, string providerMail1, string providerMail2, DateTime startTime, DateTime endTime, string websiteURL, String footerUrl)
         {
             Id = id;
             ProviderName1 = providerName1;
@@ -20,6 +20,7 @@ namespace NMHD_DataAccess.Models
             StartTime = startTime;
             EndTime = endTime;
             WebsiteURL = websiteURL;
+            FooterImageUrl = footerUrl;
         }
 
         public int Id { get; set; }
@@ -35,6 +36,7 @@ namespace NMHD_DataAccess.Models
         public DateTime EndTime { get; set; }
         public String WebsiteURL { get; set; }
         public String Address { get; set; }
+        public String FooterImageUrl { get; set; }
 
 
     }
